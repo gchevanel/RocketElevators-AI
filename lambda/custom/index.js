3d513a212b8f549cef377bd2c6740c963443bffd
Gchevanel <gchevanel@hotmail.com>
2019-04-25 15:59:59 -0400

    first commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,304 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require("ask-sdk-core");
+
+const GetLaunchHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+  },
+  handle(handlerInput) {
+    const speechText = "Hello there. How can i help you today?";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt()
+      .getResponse();
+  }
+};
+
+const GetStatusHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.intent.name === "GetStatusIntent"
+    );
+  },
+  async handle(handlerInput) {
+    let outputSpeech = "This is the default message.";
+    const id = handlerInput.requestEnvelope.request.intent.slots.id.value;
+
+    const elevatorStatus = await getRemoteElevatorData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/elevator/" + id
+    );
+
+    const elevator = JSON.parse(elevatorStatus).status;
+
+    // const customer = JSON.parse(c);
+
+    outputSpeech = `Thew status of elevator ${id} is ${elevator} `;
+
+    return handlerInput.responseBuilder
+      .speak(outputSpeech)
+      .reprompt()
+      .getResponse();
+  }
+};
+
+const GetRemoteDataHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "GetRemoteDataIntent"
+    );
+  },
+  async handle(handlerInput) {
+    let outputSpeech = "This is the default message.";
+
+    const elevatorData = await getRemoteElevatorData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/elevator/"
+    );
+    const elevatorAllData = await getRemoteElevatorAllData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/elevator/all"
+    );
+    const buildingData = await getRemoteBuildingData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/building/"
+    );
+    const customerData = await getRemoteCustomerData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/customer/"
+    );
+    const batteryData = await getRemoteBatteryData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/battery/"
+    );
+    const AddressCityData = await getRemoteCityData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/address/"
+    );
+    const QuoteData = await getQuoteData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/quote/quote"
+    );
+    const LeadData = await getLeadData(
+      "https://rocketelevatorsgab.azurewebsites.net/api/lead"
+    );
+    const elevator = JSON.parse(elevatorData);
+    const elevatorAll = JSON.parse(elevatorAllData);
+    const building = JSON.parse(buildingData);
+    const customer = JSON.parse(customerData);
+    const batteries = JSON.parse(batteryData);
+    const cities = JSON.parse(AddressCityData);
+    const quotes = JSON.parse(QuoteData);
+    const leads = JSON.parse(LeadData);
+    // const customer = JSON.parse(c);
+
+    outputSpeech = `Hi Sir welcome to Rocket Elevator Statistic. There are currently ${
+      elevatorAll.length
+    } elevators deployed in the ${building.length} buildings of your ${
+      customer.length
+    } customers . Currently, ${
+      elevator.length
+    } elevators are not in Running Status and are being serviced.
+    ${batteries.length} Battreries are deployed across ${
+      cities.length
+    } cities. On another note you currently have ${
+      quotes.length
+    } quotes awaiting processing. You also have ${
+      leads.length
+    } leads in your contact requests
+
+
+
+    `;
+
+    return handlerInput.responseBuilder
+      .speak(outputSpeech)
+      .reprompt()
+      .getResponse();
+  }
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "You can introduce yourself by telling me your name";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  }
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name ===
+        "AMAZON.CancelIntent" ||
+        handlerInput.requestEnvelope.request.intent.name ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "Goodbye!";
+
+    return handlerInput.responseBuilder.speak(speechText).getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+  },
+  handle(handlerInput) {
+    console.log(
+      `Session ended with reason: ${
+        handlerInput.requestEnvelope.request.reason
+      }`
+    );
+
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak("Sorry, I can't understand the command. Please say again.")
+      .reprompt("Sorry, I can't understand the command. Please say again.")
+      .getResponse();
+  }
+};
+const getLeadData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteElevatorData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteElevatorAllData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getQuoteData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteBuildingData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteCityData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteCustomerData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+const getRemoteBatteryData = function(url) {
+  return new Promise((resolve, reject) => {
+    const client = url.startsWith("https") ? require("https") : require("http");
+    const request = client.get(url, response => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error("Failed with status code: " + response.statusCode));
+      }
+      const body = [];
+      response.on("data", chunk => body.push(chunk));
+      response.on("end", () => resolve(body.join("")));
+    });
+    request.on("error", err => reject(err));
+  });
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetLaunchHandler,
+    GetRemoteDataHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    GetStatusHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
